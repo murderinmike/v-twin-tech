ef8f2635085c7c1b1df822df4cb9e181081cf3e0
murderinmike <miguelferreira_sjm@hotmail.com>
2026-05-27 21:13:27 +0100

    Add files via upload

--- welcome.py
+++ welcome.py
@@ -212,7 +212,8 @@ elif st.session_state["page"] == "brain":
     st.markdown("---")
     
     st.text_input("🔧 Write your message to the Mechanic:", key="campo_texto_input")
-    if st.button("🚀 Send Message to Master Tech", use_container_width=True):
-        enviar_mensagem_chat()
-        st.rerun()
+if st.button("🚀 Send Message to Master Tech", use_container_width=True):
+    enviar_mensagem_chat()
+    st.rerun()
+
 
